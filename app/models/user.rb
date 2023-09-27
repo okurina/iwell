5b25c43fe89495915334141365f4d26d50a27405
okurina <rio.60821023@gmail.com>
2023-09-27 12:46:06 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :user_name
+    validates :department
+  end
+
+  validates :login_id, presence: true, numericality: { message: 'には8桁の半角数字を使用してください' }
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角英字と半角数字の両方を含めて設定してください'
+
+  has_many :items
+  has_many :order_items
+  belongs_to :chats
 end

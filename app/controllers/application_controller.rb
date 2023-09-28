6be849da6af28a982bb3cd7328f76304590a01c2
okurina <rio.60821023@gmail.com>
2023-09-28 18:34:22 +0900

    アカウント編集機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :department, :login_num])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :department, :login_num])
+
   end
 end
   

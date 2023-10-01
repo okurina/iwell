b17d84fcea23e509c319fa76b179e3a8c8b0e0f5
okurina <rio.60821023@gmail.com>
2023-10-01 16:25:16 +0900

    商品登録機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,17 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
+  def new
+    if user_signed_in?
+      @item = Item.new
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+  def create
+  end
+
   private
 
   def move_to_index

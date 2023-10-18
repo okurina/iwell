6384ea643160b320f68aa3e000444d3e0a3ea836
okurina <rio.60821023@gmail.com>
2023-10-18 18:53:34 +0900

    プレビュー機能修正

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,27 +1,38 @@
-if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
-  document.addEventListener('DOMContentLoaded', function(){
-    const ImageList = document.getElementById('previews');
+document.addEventListener('turbo:load', function(){
+  // 新規投稿・編集ページのフォームを取得
+  const postForm = document.getElementById('new_item');
 
-    const createImageHTML = (blob) => {
-       // 画像を表示するためのdiv要素を生成
-      const imageElement = document.createElement('div');
-      // 表示する画像を生成
-      const blobImage = document.createElement('img');
-        blobImage.className="preview"; //←createElementで生成したimgにクラス名を付けている
-      blobImage.setAttribute('src', blob);
-      // 生成したHTMLの要素をブラウザに表示させる
-      imageElement.appendChild(blobImage);
-      ImageList.appendChild(imageElement);
+  // プレビューを表示するためのスペースを取得
+  const previewList = document.getElementById('previews');
+
+  // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
+  if (!postForm) return null;
+
+  // input要素を取得
+  const fileField = document.querySelector('input[type="file"]');
+  // input要素で値の変化が起きた際に呼び出される関数
+  fileField.addEventListener('change', function(e){
+    // 古いプレビューが存在する場合は削除
+    const alreadyPreview = document.querySelector('.preview');
+    if (alreadyPreview) {
+      alreadyPreview.remove();
     };
-    document.getElementById('item-image').addEventListener('change', function(e){
-      // 画像が表示されている場合のみ、すでに存在している画像を削除する
-      const imageContent = document.querySelector('.preview');
-      if (imageContent){
-        imageContent.remove();
-      }
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
-      createImageHTML(blob);
-    });
+
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    // 画像を表示するためのdiv要素を生成
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class', 'preview');
+
+    // 表示する画像を生成
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class', 'preview-image');
+    previewImage.setAttribute('src', blob);
+
+    // 生成したHTMLの要素をブラウザに表示させる
+    
+    previewWrapper.appendChild(previewImage);
+    previewList.appendChild(previewWrapper);
   });
-}
+});
